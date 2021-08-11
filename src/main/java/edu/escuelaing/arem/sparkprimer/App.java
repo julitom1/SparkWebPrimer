e2f0767bb70c3f7e73accfb6c22d9867b75fb7f6
julitom1 <yarit.villalobos@mail.escuelaing.edu.co>
2021-08-11 17:42:21 -0500

    correr en JAVA

--- src/main/java/edu/escuelaing/arem/sparkprimer/App.java
+++ src/main/java/edu/escuelaing/arem/sparkprimer/App.java
@@ -1,5 +1,7 @@
 package edu.escuelaing.arem.sparkprimer;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		get("/hello", (req, res) -> "Hello World");
     }
 }
